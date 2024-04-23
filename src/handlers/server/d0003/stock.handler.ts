442819993c5e88bcfcaca3f02407951cd4597fd8
zhangxiangliang <admin@pushme.top>
2024-04-23 22:23:38 +0800

    feat: stock message setup

--- src/handlers/server/d0003/stock.handler.ts
+++ src/handlers/server/d0003/stock.handler.ts
@@ -1,4 +1,5 @@
 import { Context } from "hono";
+import { stocks } from "stock-api";
 import { sendMessage } from "../../../utils/wechat";
 
 export const SERVER_TYPE_D0003 = "D0003";
@@ -7,8 +8,17 @@ export const serverD0003StockHandler = async (c: Context) => {
   const body = await c.req.json();
 
   const { data } = body;
+  const message = String(data.msg);
 
-  await sendMessage({ wxid: data.fromWxid, msg: "hello world" });
+  const code = message.replace("*stock", "").trim();
+  const stock = await stocks.netease.getStock(code);
+
+  const msg =
+    stock.name === "---"
+      ? "没有找到 stock 相关信息"
+      : `名称: ${stock.name}, 现价: ${stock.now}, 最低价: ${stock.low}, 最高价: ${stock.high}, 昨日收盘价格: ${stock.yesterday}`;
+
+  await sendMessage({ wxid: data.fromWxid, msg });
 
   return c.json({
     type: SERVER_TYPE_D0003,
